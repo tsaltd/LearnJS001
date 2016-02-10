fc7319ab0ea4583f7e9bb81f419bbea2bb064fd7
tsaltd <wp710012@gmail.com>
2016-02-10 02:38:41 -0500

    V0

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Middleware;
+
+namespace LearnJS.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            Middleware.JsLabViewModel model = new JsLabViewModel();
+           
+            return View(model);
+        }
+    }
+}
